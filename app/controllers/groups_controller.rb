0cf9235e3cff263418f2307f248a45bffb0d1915
leesung04@163.com <leesung04@qitiandashengdeiMac.local>
2017-04-11 16:05:22 +0800

    implement my groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,6 +6,7 @@ class Account::GroupsController < ApplicationController
   end
 end
 
+
   def show
     @group = Group.find(params[:id])
     @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
